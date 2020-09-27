71800c0a0cf78efe916b8c7522568565d2730f70
Omar Saucedo <omar.saucedo@wizeline.com>
2020-09-27 17:00:14 -0500

    add shadow to header

--- src/components/Header/Header.component.jsx
+++ src/components/Header/Header.component.jsx
@@ -8,8 +8,13 @@ const Container = styled.header`
   background: ${(props) => props.theme.colors.primary};
   display: flex;
   justify-content: space-between;
+  margin-bottom: 2rem;
   padding: 1rem;
   width: 100%;
+
+  -webkit-box-shadow: 0px 0px 50px -10px rgba(0, 0, 0, 0.75);
+  -moz-box-shadow: 0px 0px 50px -10px rgba(0, 0, 0, 0.75);
+  box-shadow: 0px 0px 50px -10px rgba(0, 0, 0, 0.75);
 `;
 
 const HeaderSection = styled.div`
