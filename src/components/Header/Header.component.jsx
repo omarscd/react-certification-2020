b8b7a47ed25f92f787178c2e8b42395b83d16a76
Omar Saucedo <omar.saucedo@wizeline.com>
2020-09-27 14:17:39 -0500

    styled-components in header

--- src/components/Header/Header.component.jsx
+++ src/components/Header/Header.component.jsx
@@ -2,34 +2,35 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
+const Container = styled.header`
+  background: ${(props) => props.theme.colors.primary};
+  display: flex;
+  justify-content: space-between;
+  padding: 1rem 2rem;
+  width: 100%;
+`;
+
+const HeaderSection = styled.div`
+  display: flex;
+`;
+
+const HeaderElement = styled.div`
+  margin: 0;
+  padding: 0 1rem;
+`;
+
+const NavLink = styled(Link)`
+  color: white;
+  font-weight: bold;
+  text-decoration: none;
+
+  :hover {
+    color: #dedede;
+    transition: color 0.3s ease-in-out;
+  }
+`;
+
 const Header = () => {
-  const Container = styled.header`
-    background: red;
-    display: flex;
-    justify-content: space-between;
-    padding: 1rem 2rem;
-    width: 100%;
-  `;
-
-  const HeaderSection = styled.div`
-    display: flex;
-  `;
-
-  const HeaderElement = styled.div`
-    margin: 0;
-    padding: 0 1rem;
-  `;
-
-  const NavLink = styled(Link)`
-    color: white;
-    text-decoration: none;
-
-    :hover {
-      color: #dedede;
-      transition: color 0.3s ease-in-out;
-    }
-  `;
-
   return (
     <Container>
       <HeaderSection>
