be4e4ac649a3ecd53b32cd09517436059e8688be
Omar Saucedo <omar.saucedo@wizeline.com>
2020-09-27 21:38:35 -0500

    update not found page

--- src/pages/NotFound/NotFound.page.jsx
+++ src/pages/NotFound/NotFound.page.jsx
@@ -1,18 +1,36 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
 import Layout from '../../components/Layout';
-import './NotFound.styles.scss';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+`;
+
+const Message = styled.p`
+  font-size: ${(props) => props.theme.fontSizes.medium};
+`;
+
+const HomeLink = styled(Link)`
+  color: ${(props) => props.theme.colors.font};
+  font-size: ${(props) => props.theme.fontSizes.small};
+  font-weight: bold;
+  transition: color 0.3s ease-in-out;
+`;
 
 function NotFoundPage() {
   return (
     <Layout>
-      <section className="not-found">
-        <Link to="/" className="home-link">
-          home
-        </Link>
-        <img src="/404.gif" alt="page not found" />
-      </section>
+      <Container>
+        <div>
+          <img src="/404.gif" alt="page not found" />
+        </div>
+        <Message>This is not the page you are lookin for</Message>
+        <HomeLink to="/">Return to home page</HomeLink>
+      </Container>
     </Layout>
   );
 }
